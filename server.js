3ad09f6ce5bf7b69e5f4cc394676ed2a8ba21210
huwngnguyeexn-1204 <thanhhung2k04@gmail.com>
2026-02-23 17:01:15 +0700

    Update server.js

--- server.js
+++ server.js
@@ -91,4 +91,4 @@ app.get('/monitor', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`🚀 Server is running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`🚀 Server is running on port ${PORT}`));
